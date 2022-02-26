ca3e2936afd26726129616742c71692f69a58015
Andrey Grytsenko <andreyapg@gmail.com>
2022-02-26 12:13:02 +0200

    Articles controller, route and page

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class ArticleController extends AbstractController
+{
+    /**
+     * @route("/articles")
+     */
+    public function index()
+    {
+        return $this->render('article.html.twig', [
+            'year' => date('Y')
+        ]);
+    }
+}
