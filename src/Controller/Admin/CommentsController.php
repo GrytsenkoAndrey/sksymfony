d100ec50d6aaa8ab7a046f5a28dfbc38da55f488
Andrey Grytsenko <andreyapg@gmail.com>
2022-06-13 11:45:55 +0300

    Add new base_content.html.twig template 2022-06-13

---
+++ src/Controller/Admin/CommentsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentsController extends AbstractController
+{
+    /**
+    * @route("/admin/comments", name="app_admin_comments")
+    */
+    public function index(): Response
+    {
+        $comments = [
+            [
+                'articleTitle' => 'Is there anything after 9 life?',
+                'comment' => 'Comment 1',
+                'createdAt' => new \DateTime('-1 hours'),
+                'authorName' => 'Funny'
+            ],
+            [
+                'articleTitle' => 'Where is my food?',
+                'comment' => 'Comment 2',
+                'createdAt' => new \DateTime('-1 days'),
+                'authorName' => 'Cat'
+            ],
+            [
+                'articleTitle' => 'What to do if you are boring?',
+                'comment' => 'Comment 3',
+                'createdAt' => new \DateTime('-3 days'),
+                'authorName' => 'Symba'
+            ],
+        ];
+
+        return $this->render('admin/comments/index.html.twig', [
+            'comments' => $comments,
+        ]);
+    }
+}
