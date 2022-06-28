e6004669be5629d9839f720e20e21905a6be2841
Andrey Grytsenko <andreyapg@gmail.com>
2022-06-28 16:11:25 +0300

    Twig: put service into separate class (LazyLoad) 2022-06-28

---
+++ src/Twig/AppRuntime.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Service\MarkdownParser;
+use Twig\Extension\RuntimeExtensionInterface;
+
+class AppRuntime implements RuntimeExtensionInterface
+{
+    /**
+     * @var MarkdownParser
+     */
+    private $markdownParser;
+
+    public function __construct(MarkdownParser $markdownParser)
+    {
+        $this->markdownParser = $markdownParser;
+    }
+
+    public function parseMarkdown($content)
+    {
+        return $this->markdownParser->parse($content);
+    }
+}
--- src/Twig/CachedMarkdownExtension.php
+++ src/Twig/CachedMarkdownExtension.php
@@ -2,35 +2,18 @@
 
 namespace App\Twig;
 
-use App\Service\MarkdownParser;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
-use Twig\TwigFunction;
 
 class CachedMarkdownExtension extends AbstractExtension
 {
-    /**
-     * @var MarkdownParser
-     */
-    private $markdownParser;
-
-    public function __construct(MarkdownParser $markdownParser)
-    {
-        $this->markdownParser = $markdownParser;
-    }
-
     public function getFilters(): array
     {
         return [
             // If your filter generates SAFE HTML, you should add a third
             // parameter: ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
-            new TwigFilter('cached_markdown', [$this, 'parseMarkdown'], ['is_safe' => ['html']]),
+            new TwigFilter('cached_markdown', [AppRuntime::class, 'parseMarkdown'], ['is_safe' => ['html']]),
         ];
-    }
-
-    public function parseMarkdown($content)
-    {
-        return $this->markdownParser->parse($content);
     }
 }
