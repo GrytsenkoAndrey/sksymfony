cc87ccbdcf1df163232d924b0d06afa34e21bb74
Andrey Grytsenko <andreyapg@gmail.com>
2022-06-28 12:23:49 +0300

    Twig: use custom Twig filter - ago() - 2022-06-28

--- src/Controller/Admin/CommentsController.php
+++ src/Controller/Admin/CommentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use Carbon\Carbon;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,22 +21,24 @@ class CommentsController extends AbstractController
             [
                 'articleTitle' => 'Is there anything after 9 life?',
                 'comment' => 'Comment 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                'createdAt' => new \DateTime('-1 hours'),
+                'created_at' => new \DateTime('-1 hours'),
                 'authorName' => 'Funny'
             ],
             [
                 'articleTitle' => 'Where is my food?',
                 'comment' => 'Value 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                'createdAt' => new \DateTime('-1 days'),
+                'created_at' => new \DateTime('-1 days'),
                 'authorName' => 'Cat'
             ],
             [
                 'articleTitle' => 'What to do if you are boring?',
                 'comment' => 'Comment 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                'createdAt' => new \DateTime('-3 days'),
+                'created_at' => new \DateTime('-3 days'),
                 'authorName' => 'Symba'
             ],
         ];
+
+        #dd((new Carbon($comments[0]['created_at']))->locale('ru')->diffForHumans());
 
         if ($q) {
             $q = strip_tags($q);
