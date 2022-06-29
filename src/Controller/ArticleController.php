2f5721e0ff6864852b98849e7c342a90ea9877b3
Andrey Grytsenko <andreyapg@gmail.com>
2022-06-29 15:43:07 +0300

    Use ArticleRepository instead of EntityManagerInterface 2022-06-29

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Article;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +12,9 @@ final class ArticleController extends AbstractController
     /**
      * @route("/articles", name="app_articles")
      */
-    public function index(EntityManagerInterface $em): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
-        $repository = $em->getRepository(Article::class);
-        $articles = $repository->findLatestPublished();
+        $articles = $articleRepository->findLatestPublished();
 
         return $this->render('article.html.twig', [
             'year' => date('Y'),
@@ -28,10 +26,9 @@ final class ArticleController extends AbstractController
     /**
      * @route("/articles/{slug}", name="app_article_show")
      */
-    public function show(string $slug, EntityManagerInterface $em): Response
+    public function show(string $slug, ArticleRepository $articleRepository): Response
     {
-        $repository = $em->getRepository(Article::class);
-        $article = $repository->findOneBy(['slug' => $slug]);
+        $article = $articleRepository->findOneBy(['slug' => $slug]);
 
         if (! $article) {
             throw $this->createNotFoundException(sprintf('Article with slug %s not found', $slug));
