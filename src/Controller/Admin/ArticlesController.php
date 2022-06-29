41f06dafeb4008741f0d2f31cad177a443e1797e
Andrey Grytsenko <andreyapg@gmail.com>
2022-06-29 12:32:07 +0300

    Add ArticleController store(), with correction 2022-06-29

--- src/Controller/Admin/ArticlesController.php
+++ src/Controller/Admin/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,7 +19,7 @@ class ArticlesController extends AbstractController
     }
 
     #[Route('/admin/articles/create', name: 'app_admin_articles_create')]
-    public function create(): Response
+    public function create(EntityManagerInterface $entityManager): Response
     {
         /*return $this->render('admin/articles/create.html.twig', [
             'controller_name' => 'ArticlesController',
@@ -31,9 +32,16 @@ class ArticlesController extends AbstractController
             ->setBody($postfix . ' :: It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
 
         if (rand(1, 10) > 4) {
-            $article->setPublishedAt(new \DateTimeImmutable(sprintf('-%sd days', rand(0, 30))));
+            $article->setPublishedAt(new \DateTimeImmutable(sprintf('-%d days', rand(0, 30))));
         }
 
-        return new Response('Here will be article create page');
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new Response(sprintf(
+            'Article created with id: %d slug %s',
+            $article->getId(),
+            $article->getSlug()
+        ));
     }
 }
