eb38c89dac357d933f8c433836ac1153f08ce147
Andrey Grytsenko <andreyapg@gmail.com>
2022-07-01 09:30:07 +0300

    Update fixture for 10 Articles 2022-07-01

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -11,26 +11,29 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $article = new Article();
+        for ($cnt = 10, $i = 0; $i < $cnt; $i++) {
+            $article = new Article();
 
-        $faker = Factory::create();
-        $title = ucfirst($faker->words(random_int(1, 3), true));
-        $slug = str_replace(' ', '-', strtolower($title));
+            $faker = Factory::create();
+            $title = ucfirst($faker->words(random_int(1, 3), true));
+            $slug = str_replace(' ', '-', strtolower($title));
 
-        $article
-            ->setTitle($title)
-            ->setSlug($slug)
-            ->setBody(ucfirst($faker->sentences(10, true)));
+            $article
+                ->setTitle($title)
+                ->setSlug($slug)
+                ->setBody(ucfirst($faker->sentences(10, true)));
 
-        if (rand(1, 10) > 4) {
-            $article->setPublishedAt(new \DateTimeImmutable(sprintf('-%d days', rand(0, 30))));
+            if (rand(1, 10) > 4) {
+                $article->setPublishedAt(new \DateTimeImmutable(sprintf('-%d days', rand(0, 30))));
+            }
+
+            $article->setAuthor($faker->name)
+                ->setLikeCount(random_int(1, 100))
+                ->setImageFilename('simon-hey.png');
+
+            $manager->persist($article);
         }
 
-        $article->setAuthor($faker->name)
-            ->setLikeCount(random_int(1, 100))
-            ->setImageFilename('simon-hey.png');
-
-        $manager->persist($article);
         $manager->flush();
     }
 }
