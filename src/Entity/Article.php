cfafe6956233708cc52722bd8793076139768de5
Andrey Grytsenko <andreyapg@gmail.com>
2022-07-03 09:29:15 +0300

    Use doctrine filter to show only non deleted comments 2022-07-03

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
@@ -180,11 +179,7 @@ class Article
      */
     public function getNonDeletedComments(): Collection
     {
-        $criteria = Criteria::create()
-            ->andWhere(Criteria::expr()->isNull('deletedAt'))
-            ->orderBy(['createdAt' => 'DESC']);
-
-        return $this->comments->matching($criteria);
+        return $this->comments;
     }
 
     public function addComment(Comment $comment): self
