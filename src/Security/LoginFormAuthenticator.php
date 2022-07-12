5946eac0f894d79a91cbb961ef897ae53546b9ae
Andrey Grytsenko <andreyapg@gmail.com>
2022-07-12 11:00:06 +0300

    Remove custom and add by default 2022-07-12

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -5,27 +5,29 @@ namespace App\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     protected function getLoginUrl(Request $request): string
     {
-        // TODO: Implement getLoginUrl() method.
+        return 'test';
     }
 
     public function authenticate(Request $request)
     {
-        // TODO: Implement authenticate() method.
+        dd($request->attributes);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        #dd('Here');
     }
 
-    public function supports(Request $request)
+    public function supports(Request $request): bool
     {
-
+        return $request->attributes->get('_route') === 'app_login'
+            && $request->isMethod('POST');
     }
 }
