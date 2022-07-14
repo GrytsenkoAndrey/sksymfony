b7db5a3dba9f24f25c89df79e686adb2a0e87308
Andrey Grytsenko <andreyapg@gmail.com>
2022-07-14 15:29:18 +0300

    Update UserFixtures (add hash for the password) 2022-07-14

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,16 +4,27 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends BaseFixtures
 {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {
+    }
+
     public function loadData(ObjectManager $manager): void
     {
         $this->createMany(User::class, 10, function (User $user) {
             $user
                 ->setEmail($this->faker->email)
                 ->setFirstName($this->faker->firstName)
-                ->setPassword('123456');
+                ->setPassword(
+                    $this->userPasswordHasher->hashPassword(
+                        $user,
+                        '123456'
+                    )
+                );
         });
     }
 }
