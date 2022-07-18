402af4db42d695b51e355a916e29b6b45a7de892
Andrey Grytsenko <andreyapg@gmail.com>
2022-07-18 11:31:11 +0300

    Access by Annotation 2022-07-18

--- src/Controller/Admin/CommentsController.php
+++ src/Controller/Admin/CommentsController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Repository\CommentRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,11 +14,10 @@ class CommentsController extends AbstractController
 {
     /**
     * @route("/admin/comments", name="app_admin_comments")
+     * @IsGranted("ROLE_ADMIN")
     */
     public function index(Request $request, CommentRepository $commentRepository, PaginatorInterface $paginator): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
-
         $comments = $commentRepository->findAllWithSearchQuery(
             $request->query->get('q'),
             $request->query->has('showDeleted')
